6fca69d5a1820983f47dee7f57c924b60a918c7d
Jack Chan <jackchw.uk@gmail.com>
2025-07-06 01:36:14 +0100

    create timer player

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,54 +1,173 @@
-import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
+import { Image } from "expo-image";
+import { Button, StyleSheet } from "react-native";
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import ParallaxScrollView from "@/components/ParallaxScrollView";
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedView } from "@/components/ThemedView";
+
+import { useAudioPlayer } from "expo-audio";
+import { Directory, File, Paths } from "expo-file-system/next";
+import { useRef, useState } from "react";
+
+import DateTimePicker from "@react-native-community/datetimepicker";
+
+const getDir = () => {
+  const destination = new Directory(Paths.document, "audios");
+  if (!destination.exists) {
+    destination.create();
+  }
+  return destination;
+};
+
+const Player = ({ fileName }: { fileName: string }) => {
+  const [showTimePicker, setShowTimePicker] = useState(false);
+  const [playDate, setPlayDate] = useState<Date>(new Date());
+  const [untilTime, setUntilTime] = useState<number>();
+  const clockIntervalRef = useRef<number>(null);
+  const clockTimeoutRef = useRef<number>(null);
+
+  const onChange = (event: any, selectedDate?: Date) => {
+    setShowTimePicker(false);
+    if (!selectedDate) return;
+    const currentDate = selectedDate;
+    if (currentDate.getTime() - Date.now() < 0) {
+      return;
+    }
+    setPlayDate(currentDate);
+  };
+
+  const direactory = getDir();
+  const file = new File(direactory, fileName);
+  const player = useAudioPlayer(file);
+
+  const updateCurrentDate = () => {
+    const currentTime = Date.now();
+    const diff = playDate.getTime() - currentTime;
+    if (diff < 0) return;
+    if (diff < 10000 && clockTimeoutRef.current == null) {
+      clockTimeoutRef.current = setTimeout(playAudio, diff);
+    }
+    setUntilTime(diff);
+  };
+
+  const clearClock = () => {
+    if (clockIntervalRef.current) {
+      clearInterval(clockIntervalRef.current);
+    }
+  };
+
+  const startCountDown = async () => {
+    if (clockIntervalRef.current) {
+      clearInterval(clockIntervalRef.current);
+    }
+
+    if (playDate.getTime() - Date.now() < 0) {
+      return;
+    }
+
+    clockIntervalRef.current = setInterval(updateCurrentDate, 1000);
+    await player.seekTo(0);
+  };
+
+  const playAudio = async () => {
+    player.play();
+
+    if (clockIntervalRef.current) {
+      clearInterval(clockIntervalRef.current);
+    }
+    if (clockTimeoutRef.current) {
+      clearTimeout(clockTimeoutRef.current);
+    }
+  };
+
+  return (
+    <ThemedView>
+      <Button onPress={() => setShowTimePicker(true)} title="Start At" />
+
+      {showTimePicker && (
+        <DateTimePicker
+          testID="dateTimePicker"
+          value={playDate}
+          mode="time"
+          is24Hour={true}
+          onChange={onChange}
+        />
+      )}
+
+      <Button onPress={startCountDown} title="Play"></Button>
+      {clockIntervalRef.current && (
+        <Button onPress={clearClock} title="Reset"></Button>
+      )}
+      {untilTime && (
+        <ThemedView>
+          <ThemedText>Start in</ThemedText>
+          <ThemedText>{Math.floor(untilTime / 1000 / 60)}m</ThemedText>
+          <ThemedText>{Math.floor((untilTime / 1000) % 60)}s</ThemedText>
+        </ThemedView>
+      )}
+    </ThemedView>
+  );
+};
+
+const fileName = "001.m4a";
 
 export default function HomeScreen() {
+  const [downloadStatus, setDownloadStatus] = useState<
+    undefined | "loading" | "success"
+  >(undefined);
+  const [isExist, setIsExist] = useState(() => {
+    const directory = getDir();
+    const file = new File(directory, fileName);
+    return file.exists;
+  });
+  const download = async () => {
+    setDownloadStatus("loading");
+    const url = `${process.env.EXPO_PUBLIC_API_URL}/${fileName}`;
+
+    const destination = getDir();
+    const output = await File.downloadFileAsync(url, destination);
+    setDownloadStatus("success");
+    setIsExist(true);
+  };
+
+  const deleteFile = () => {
+    if (!isExist) return;
+    const directory = getDir();
+    const file = new File(directory, fileName);
+    file.delete();
+    setIsExist(false);
+    setDownloadStatus(undefined);
+  };
+
   return (
     <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
+      headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
       headerImage={
         <Image
-          source={require('@/assets/images/partial-react-logo.png')}
+          source={require("@/assets/images/partial-react-logo.png")}
           style={styles.reactLogo}
         />
-      }>
+      }
+    >
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
+        <ThemedText type="title">Speaker</ThemedText>
       </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
+      <Button
+        onPress={download}
+        title="Download"
+        disabled={downloadStatus != undefined || isExist}
+      />
+
+      <Button onPress={deleteFile} title="Delete" disabled={!isExist} />
+      {isExist && <Player fileName={fileName} />}
+
+      <ThemedView style={styles.titleContainer}>
         <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12',
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          {`Tap the Explore tab to learn more about what's included in this starter app.`}
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          {`When you're ready, run `}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
+          {downloadStatus == "loading"
+            ? "Downloading"
+            : downloadStatus == "success"
+            ? "Download Successful"
+            : ""}
         </ThemedText>
       </ThemedView>
     </ParallaxScrollView>
@@ -57,8 +176,8 @@ export default function HomeScreen() {
 
 const styles = StyleSheet.create({
   titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
     gap: 8,
   },
   stepContainer: {
@@ -70,6 +189,6 @@ const styles = StyleSheet.create({
     width: 290,
     bottom: 0,
     left: 0,
-    position: 'absolute',
+    position: "absolute",
   },
 });
